5660b7a9bcbfe0d8b663be7693ad9ab4aa24dc0e
Puteri Yudani <puteriyudani@gmail.com>
2022-05-07 22:55:59 +0700

    Menggunakan Kelas Handler

--- app/src/main/java/com/puteriyudani/threadrunnable/MainActivity.kt
+++ app/src/main/java/com/puteriyudani/threadrunnable/MainActivity.kt
@@ -2,26 +2,31 @@ package com.puteriyudani.threadrunnable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mhandler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        mhandler = object : Handler() {
+            override fun handleMessage(msg: Message) {
+                textView.text = msg?.data?.getString("counter")
+            }
+        }
         button.setOnClickListener {
             Thread(Runnable {
                 killSomeTime()
             }).start()
         }
     }
-
     private fun killSomeTime() {
         for (i in 1..20) {
-            runOnUiThread(Runnable{
-                textView.text = i.toString()
-            })
-            println("i:$i")
+            var msg = Message.obtain()
+            msg.data.putString("counter", i.toString())
+            mhandler.sendMessage(msg)
             Thread.sleep(2000)
         }
     }
