48aa16e5a374bbe3fdd4d574251323b790a064bf
Puteri Yudani <puteriyudani@gmail.com>
2022-05-07 22:58:34 +0700

    AsyncTask

--- app/src/main/java/com/puteriyudani/threadrunnable/MainActivity.kt
+++ app/src/main/java/com/puteriyudani/threadrunnable/MainActivity.kt
@@ -1,33 +1,31 @@
 package com.puteriyudani.threadrunnable
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mhandler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mhandler = object : Handler() {
-            override fun handleMessage(msg: Message) {
-                textView.text = msg?.data?.getString("counter")
-            }
-        }
         button.setOnClickListener {
-            Thread(Runnable {
-                killSomeTime()
-            }).start()
+            Worker().execute()
         }
     }
-    private fun killSomeTime() {
-        for (i in 1..20) {
-            var msg = Message.obtain()
-            msg.data.putString("counter", i.toString())
-            mhandler.sendMessage(msg)
-            Thread.sleep(2000)
+    inner class Worker : AsyncTask<Void, String, Boolean>() {
+        override fun doInBackground(vararg p0: Void?): Boolean {
+            for (i in 1..20) {
+                publishProgress(i.toString())
+                Thread.sleep(2000)
+            }
+            return true
+        }
+        override fun onProgressUpdate(vararg values: String?) {
+            textView.text = values[0]
+        }
+        override fun onPostExecute(result: Boolean?) {
+            println(result)
         }
     }
 }
