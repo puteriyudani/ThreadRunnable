4c88c32f9e24ccea612d2b36d916c4a1dcd438fd
Puteri Yudani <puteriyudani@gmail.com>
2022-05-07 23:01:50 +0700

    Anko’s doAsync

--- app/src/main/java/com/puteriyudani/threadrunnable/MainActivity.kt
+++ app/src/main/java/com/puteriyudani/threadrunnable/MainActivity.kt
@@ -1,31 +1,24 @@
 package com.puteriyudani.threadrunnable
 
-import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.activityUiThread
+import org.jetbrains.anko.doAsync
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button.setOnClickListener {
-            Worker().execute()
-        }
-    }
-    inner class Worker : AsyncTask<Void, String, Boolean>() {
-        override fun doInBackground(vararg p0: Void?): Boolean {
-            for (i in 1..20) {
-                publishProgress(i.toString())
-                Thread.sleep(2000)
+            doAsync {
+                for(i in 1..15) {
+                    Thread.sleep(2000)
+                    activityUiThread {
+                        textView.text = i.toString()
+                    }
+                }
             }
-            return true
-        }
-        override fun onProgressUpdate(vararg values: String?) {
-            textView.text = values[0]
-        }
-        override fun onPostExecute(result: Boolean?) {
-            println(result)
         }
     }
 }
